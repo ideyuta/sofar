13692d7b8043ab477adb30e595e33d1c5c0ed606
ideyuta <ide.yuta@gmail.com>
2016-02-03 22:04:20 +0900

    feat(front): Add GA

--- html.jsx
+++ html.jsx
@@ -54,6 +54,7 @@ export default class Html extends React.Component {
         <body>
           <div id="react-mount" />
           <script src={link('/bundle.js')} />
+          <script dangerouslySetInnerHTML={{__html: '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create", "UA-36151317-1", "auto");ga("send", "pageview");'}} />
           <script
             async={true}
             deffer={true}
