346b445a6e0bee84d0455f28bb4d3e3b5231a71b
ideyuta <ide.yuta@gmail.com>
2016-02-08 07:49:49 +0900

    feat(components): Add Header to Tmpl

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {RouteHandler} from 'react-router';
+import Header from '../components/Header';
 import 'public/bundle.css';
+
+const propTypes = {
+  config: PropTypes.object.isRequired
+};
 
 /**
  * Tmpl Component
@@ -12,6 +17,13 @@ export default class Tmpl extends React.Component {
    * @return {ReactElement}
    */
   render() {
-    return <RouteHandler {...this.props} />;
+    return (
+      <div>
+        <Header blogTitle={this.props.config.blogTitle}/>
+        <RouteHandler {...this.props} />
+      </div>
+    );
   }
 }
+
+Tmpl.propTypes = propTypes;
