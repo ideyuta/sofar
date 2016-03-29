37b6d40ad9c8fe71c557027ab5f3cd8b9dfb3002
ideyuta <ide.yuta@gmail.com>
2016-03-29 09:03:10 +0900

    fix(html): Fix eslint warning :warning:

--- html.js
+++ html.js
@@ -55,7 +55,9 @@ export default function Html(props) {
           id="react-mount"
         />
         <script src={link('/bundle.js')} />
-        <script dangerouslySetInnerHTML={{__html: '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create", "UA-36151317-1", "auto");ga("send", "pageview");'}} />
+        <script
+          dangerouslySetInnerHTML={{__html: '(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,"script","//www.google-analytics.com/analytics.js","ga");ga("create", "UA-36151317-1", "auto");ga("send", "pageview");'}} // eslint-disable-line max-len
+        />
         <script
           async={true}
           deffer={true}
